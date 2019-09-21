30d82e910b54cec2214a55a1b07816354de13a95
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2019-09-21 12:09:57 +0200

    .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,7 @@ module.exports = {
 		'no-unneeded-ternary': 'error' ,
 		'no-unused-vars': 'warn' ,	// During development phase, it's boring to clean unused var since they can be used later
 		'no-lonely-if': 'error' ,
-		'no-nested-ternary': 'error' ,
+		'no-nested-ternary': 'off' ,	// Now I use the streamlined ternary operator a lot
 		'no-shadow': 'warn' ,
 		'no-shadow-restricted-names': 'error' ,
 		'require-atomic-updates': 'off' ,	// check for possible race condition on assignment, interesting but too nitpicky
